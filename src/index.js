6d63bb3b1216cbe7383bf91fa1c974c2b3041822
José Carlos Araújo Pontes <jose.carlos.araujo07@aluno.ifce.edu.br>
2022-08-16 14:01:02 -0300

    corrigindo path bug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import './index.scss'
+import './Index.scss'
 
 ReactDOM.render(<App></App>, document.querySelector('#root'))
