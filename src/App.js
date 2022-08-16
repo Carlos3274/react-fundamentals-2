48dad62a3ffe71773d8d97344309f521388bdae3
José Carlos Araújo Pontes <jose.carlos.araujo07@aluno.ifce.edu.br>
2022-08-16 10:20:22 -0300

    27min

--- src/App.js
+++ src/App.js
@@ -3,47 +3,58 @@ import Post from './Post'
 import Header from './Header'
 import { ThemeProvider } from './ThemeContext'
 
-
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: Math.random(),
+      title: 'Title#01',
+      subtitle: 'Subtitle#01',
+      likes: 20,
+      read: false,
+      removed: true
+    },
+    {
+      id: Math.random(),
+      title: 'Title#02',
+      subtitle: 'Subtitle#02',
+      likes: 10,
+      read: true,
+      removed: false
+    },
+    {
+      id: Math.random(),
+      title: 'Title#03',
+      subtitle: 'Subtitle#03',
+      likes: 15,
+      read: true,
+      removed: false
+    }
+  ])
 
-  const [posts, setPosts] = useState(
-    [
-      { id: Math.random(), title: 'Title#01', subtitle: 'Subtitle#01', likes: 20, read: false },
-      { id: Math.random(), title: 'Title#02', subtitle: 'Subtitle#02', likes: 10, read: true },
-      { id: Math.random(), title: 'Title#03', subtitle: 'Subtitle#03', likes: 15, read: true }, 
-    ]
-  )
-  
   function handleRemovePost(postId) {
-    setPosts((prevState) => (
-    prevState.filter(post => post.id !== postId)
-    ))
+    setPosts(prevState => prevState.map(post => post.id === postId ? { ...post, removed: true} : post))
   }
-
-
-
+ // prevState é a lista atual de posts
   function handleRefresh() {
     setPosts(prevState => [
       ...prevState,
-      {id:Math.random(), title: `Title#0${prevState.length + 1}`, subtitle: 'Subtitle#01', likes: 20}])
+      {
+        id: Math.random(),
+        title: `Title#0${prevState.length + 1}`,
+        subtitle: 'Subtitle#01',
+        likes: 20
+      }
+    ])
   }
   return (
-    <ThemeProvider> 
-      <Header
-      title = "Zeca's Blog">
-        Novidades de 2022
-        
-      </Header>
+    <ThemeProvider>
+      <Header title="Zeca's Blog">Novidades de 2022</Header>
       <button onClick={handleRefresh}>Atualizar</button>
-      <hr/>
+      <hr />
 
-      {posts.map(post => 
-        <Post 
-          key={post.id}
-          onRemove = {handleRemovePost}
-          post = {post}
-        />
-      )}
+      {posts.map(post => (
+        <Post key={post.id} onRemove={handleRemovePost} post={post} />
+      ))}
     </ThemeProvider>
   )
 }
--- src/Header.js
+++ src/Header.js
@@ -3,23 +3,14 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 import { ThemeContext } from './ThemeContext'
 import styles from './Header.scss'
-<<<<<<< HEAD
-function Header(props) {
-  const { onToggleTheme } = useContext(ThemeContext)
-  return (
-    <div className={styles.title}>
-      <h1>{props.title}</h1>
-=======
-
 function Header(props) {
   const { onToggleTheme } = useContext(ThemeContext)
   return (
     <>
       <h1 className={styles.title}>{props.title}</h1>
->>>>>>> 2e3a3600564042a1d87c321f85e999ccd15b5183
       <Button onClick={onToggleTheme}>Mudar Tema</Button>
-      <hr />
-    </div>
+      {props.children}
+    </>
   )
 }
 
